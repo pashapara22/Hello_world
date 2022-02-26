5c9acc325cfe3c640954dfd89c9770eeeaeda7ae
Pasha@22 <shaik_pasha1@persistent.com>
2022-02-26 20:32:53 +0530

    Added header to all

--- math.py
+++ math.py
@@ -1,12 +1,12 @@
-#additin Func.
+#Addition Function
 def add(x,y):
     pass 
-    
+#Subtraction Function    
 def subtraction(x,y):
     pass 
-    
+#Multiplication Function    
 def multiply(x,y):
     pass 
-    
+#Division Function
 def divide(x,y):
     pass 
