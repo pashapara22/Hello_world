f7b6140f707e07f352fe049492cc592adc01708c
Pasha@22 <shaik_pasha1@persistent.com>
2022-02-26 22:51:35 +0530

    implemented MULTIPLICATION() functION FROM lOCAL_REPO

--- math.py
+++ math.py
@@ -6,7 +6,7 @@ def subtraction(x,y):   # From Remote_repo.
     return x-y 
 #Multiplication Function    
 def multiply(x,y):
-    pass 
+    return x*y      # From local_Repo 
 #Division Function
 def divide(x,y):
     pass 
